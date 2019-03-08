8f2de09afeacd790fd834c303ea906f69ba3498f
1091107 <1091107@isep.ipp.pt>
2019-03-08 15:20:26 +0000

    Fix database name

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'driverName' => 'mysql',
-    'dsn' => 'mysql:host=localhost;dbname=linkya_list',
+    'dsn' => 'mysql:host=localhost;dbname=todo_list',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
